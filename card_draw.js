847ea8800af03e4794021564507c5f49caa0fd18
MOUTHWEST <westxmouthwest@gmail.com>
2018-12-12 01:29:16 -0500

    added first textures and color scheme

---
+++ card_draw.js
@@ -0,0 +1,93 @@
+function draw_cards(){
+	let activecard = {name:'activecard'};
+	activecard.init = function(dict){
+		this.color = 'multi';
+		this.colors = {
+			red: [[255,0,0],[120,0,0],[255,30,30]],
+			pink: [[255,160,160],[200,60,120],[255,190,190]],
+			white: [[240,240,240],[150,150,170],[255,255,255]],
+			gray: [[130,130,130],[70,70,70],[200,200,170]],
+			black: [[10,10,30],[0,0,0],[60,30,20]],
+			bronze: [[130,100,50],[80,30,30],[180,130,60]],
+			orange: [[200,130,20],[130,80,0],[255,100,70]],
+			yellow: [[230,200,0],[140,160,0],[255,255,0]],
+			green: [[50,200,30],[0,120,30],[60,255,60]],
+			aqua: [[120,240,240],[60,200,160],[180,200,255]],
+			blue: [[50,50,255],[30,0,100],[100,140,255]],
+			violet: [[160,40,200],[120,20,180],[255,100,200]],
+			multi: [[200,255,255],[250,150,150],[225,255,128]],
+		}[this.color];
+		this.baseseed = 100;
+		this.draw_card_round = function(x,y,w,ctx){
+			//dims: 10 * 14
+			let fillet_ratio = 0.1;
+			let r = w * fillet_ratio;
+			let h = w * 1.4;
+			let q = Math.PI/2;
+			ctx.beginPath();
+			ctx.arc(x+r, y+r, r, 2*q, 3*q);
+			ctx.lineTo(x+w-r, y);
+			ctx.arc(x+w-r, y+r, r, 3*q, 4*q);
+			ctx.lineTo(x+w, y+h-r);
+			ctx.arc(x+w-r, y+h-r, r, 0, q);
+			ctx.lineTo(x+r, y+h)
+			ctx.arc(x+r, y+h-r, r, q, 2*q);
+			ctx.closePath();
+			ctx.fillStyle = this.color;
+			ctx.fill();
+		}
+		this.rgba_builder = function(rgba){
+			return 'rgba(' + rgba.join(',') + ')';
+		}
+		this.draw_card_square = function(x,y,w,ctx){
+			let h = w * 1.4;
+			ctx.fillStyle = this.rgba_builder([...this.colors[0],1]);
+			ctx.fillRect(x,y,w,h);
+
+			this.random = function(s){
+				v = s[0]++;
+				for(let i = 0; i < 3; i++)
+					v = (9*v)%1337 + (v*v)%1337 + Math.floor(v*9.0/1.2);
+				return (v%1337)/1337.0;
+			}
+			seed = [this.baseseed];
+			ctx.fillStyle = this.rgba_builder([...this.colors[1],this.random(seed)/4]);
+			for(let i = 0; i < 500; i++){
+				let r = this.random(seed) * this.random(seed) * w * 0.25;
+				let xx = this.random(seed) * w + x;
+				let yy = this.random(seed) * h + y;
+				ctx.beginPath();
+				ctx.arc(xx,yy,r,0,2*Math.PI);
+				ctx.closePath();
+				ctx.fill();
+			}
+			for(let i = x; i < x+w; i+=1+5*this.random(seed)){
+				for(let j = y; j < y+h; j+=1+3*this.random(seed)){
+					ctx.fillStyle = this.rgba_builder([...this.colors[2],this.random(seed)/4]);
+					if(this.random(seed) > 0.7)
+						ctx.fillRect(i,j,1+this.random(seed)*15,1+this.random(seed)*15);
+				}
+			}
+		}
+		this.draw_card_square(500,100,300,dict['ctx']);
+		this.bkgd = dict['ctx'].getImageData(500,100,300,300*1.4);
+	}
+	activecard.update = function(dict){
+		if(dict['kys'].hasOwnProperty(32) && dict['kys'][32] == 0){
+			this.baseseed += 1;
+			this.draw_card_square(500,100,300,dict['ctx']);
+			this.bkgd = dict['ctx'].getImageData(500,100,300,300*1.4);
+		}
+	}
+	activecard.draw = function(dict){
+		//draw card rectangle
+		this.draw_card_round(100,100,300,dict['ctx']);
+		//this.draw_card_square(500,100,300,dict['ctx']);
+		dict['ctx'].putImageData(this.bkgd,500,100);
+		dict['ctx'].fillStyle = 'white';
+		dict['ctx'].font = '100px monospace';
+		dict['ctx'].fillText(this.baseseed, 630,600);
+	}
+	
+	return [activecard];
+}
--- gems_engine.js
+++ gems_engine.js
@@ -8,20 +8,36 @@ let gameArea = {
 	objects : [],
 	time : 0,
 	msrefresh : 20,
+	keys : {},
 	
 	update : function() {
 		let delta = new Date() - gameArea.time * gameArea.msrefresh;
 		if(delta >= gameArea.msrefresh){
 			gameArea.time = Math.floor(new Date()/gameArea.msrefresh);
-			let ctx = gameArea.context;
-			ctx.clearRect(0, 0, gameArea.canvas.width,gameArea.canvas.height);
+			let dict = {
+				cnv: gameArea.canvas,
+				ctx: gameArea.context,
+				obj: gameArea.objects,
+				kys: gameArea.keys,
+			};
+			gameArea.context.clearRect(0, 0, gameArea.canvas.width,gameArea.canvas.height);
 			gameArea.objects.forEach(function(obj){
+				if(obj.livetime == undefined){
+					if(obj.init != undefined)
+						obj.init(dict);
+					obj.livetime = 0;
+				}
 				if(obj.update != undefined)
-					obj.update();
-			});
-			gameArea.objects.forEach(function(obj){
+					obj.update(dict);
 				if(obj.draw != undefined)
-					obj.draw(ctx);
+					obj.draw(dict);
+				obj.livetime += 1;
+			});
+			Object.keys(gameArea.keys).forEach(function(key) {
+				//console.log(key)
+				if(key in gameArea.keys){
+					gameArea.keys[key] += 1;
+				}
 			});
 		}
 		window.requestAnimationFrame(gameArea.update);
@@ -32,27 +48,37 @@ let gameArea = {
 		this.canvas.height = 720;
 		this.context = this.canvas.getContext("2d");
 		
+		this.objects = draw_cards();
 		window.requestAnimationFrame(gameArea.update);
 		
 		document.onkeydown = function(e) {
-			switch (e.keyCode) {
-
+			if(e.keyCode != 116)
+				e.preventDefault();
+			if(! (e.keyCode in gameArea.keys)){
+				gameArea.keys[e.keyCode] = 0;
 			}
 		}
 		
-		this.canvas.addEventListener('mousedown', function(event){
-			let x = event.offsetX;
-			let y = event.offsetY;
+		document.onkeyup = function(e) {
+			if(gameArea.keys.hasOwnProperty(e.keyCode)){
+				delete gameArea.keys[e.keyCode];
+			}
+		}
+		
+		this.canvas.addEventListener('mousedown', function(e){
+			e.preventDefault();
+			let x = e.offsetX;
+			let y = e.offsetY;
 		}, false);
 		
-		this.canvas.addEventListener('mouseup', function(event){
-			let x = event.offsetX;
-			let y = event.offsetY;
+		this.canvas.addEventListener('mouseup', function(e){
+			let x = e.offsetX;
+			let y = e.offsetY;
 		}, false);
 		
-		this.canvas.addEventListener('mousemove', function(event){
-			let x = event.offsetX;
-			let y = event.offsetY;
+		this.canvas.addEventListener('mousemove', function(e){
+			let x = e.offsetX;
+			let y = e.offsetY;
 		}, false);
 	}
 };
