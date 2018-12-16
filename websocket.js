595bcf9ae9457c6111410a5831ccd42fc7ef4d03
CreepaShadowz <creepashadowz@gmail.com>
2018-12-16 17:23:59 +1300

    Added specific protocol to websocket

--- websocket.js
+++ websocket.js
@@ -2,7 +2,7 @@
 
 function initialiseWebSocket(dict) {
 	// Create a new WebSocket.
-	var socket = new WebSocket('ws://35.182.156.146:8080');
+	var socket = new WebSocket("ws://35.182.156.146:8080", "echo-protocol");
 
 	// Handle any errors that occur.
 	socket.onerror = function(error) {
